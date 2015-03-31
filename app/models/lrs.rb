dedf14683c532b35ee623ca663e065ba1b9bb4f7
Paul Coleman <pcoleman@coleman.co.uk>
2015-03-31 17:35:38 +0100

    added api to Lrs

--- app/models/lrs.rb
+++ app/models/lrs.rb
@@ -2,10 +2,20 @@ class Lrs
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :title, type: String
-  field :description, type: String
-
+  field :title
+  field :description
+  field :api, type: Hash
 
   validates :title, presence: true
   validates :description, presence: true
+
+  set_callback(:validation, :before) do |document|
+    unless document.api
+      document.api = {
+          basic_key: SecureRandom.hex(20),
+          basic_secret: SecureRandom.hex(20)
+      }
+    end
+  end
+
 end
