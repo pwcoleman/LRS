12e31f78aacc8c05bddcb005ca2dfa7bc933e8ff
Paul Coleman <pcoleman@coleman.co.uk>
2015-04-08 10:57:36 +0100

    added Retrieve state ids feature

---
+++ features/step_definitions/xapi/state/retrieve_state_ids_steps.rb
@@ -0,0 +1,25 @@
+# encoding: UTF-8
+Given(/^a \[typical\] retrieveStateIds request cluster$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the retrieveStateIds response is verified$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a \[withRegistration\] retrieveStateIds request cluster$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a \[typical\] retrieveStateIds request$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a \[withRegistration\] retrieveStateIds request$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a typical retrieveStateIds request$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
