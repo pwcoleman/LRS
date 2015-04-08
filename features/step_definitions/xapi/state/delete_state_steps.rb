3abe2cdc11f64ffde7b9d0d4cb6f6113a72d8c35
Paul Coleman <pcoleman@coleman.co.uk>
2015-04-08 10:33:41 +0100

    added Delete state feature

---
+++ features/step_definitions/xapi/state/delete_state_steps.rb
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+Given(/^a \[typical\] deleteState request cluster$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the deleteState response is verified$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a \[withRegistration\] deleteState request cluster$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^all requests' stateId parameter are set to 'test state id'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a \[typical\] deleteState request$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a \[withRegistration\] deleteState request$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a typical deleteState request$/) do
+  pending # express the regexp above with the code you wish you had
+end
