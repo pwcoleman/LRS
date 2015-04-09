c26f353564f16fcf982406f6186020697a4a71cc
Paul Coleman <pcoleman@coleman.co.uk>
2015-04-09 11:17:39 +0100

    added statement structure object agent feature

---
+++ features/step_definitions/xapi/statement_structure/object_agent_steps.rb
@@ -0,0 +1,81 @@
+# encoding: UTF-8
+Given(/^the statement object is changed to a \[mboxAndType agent\]$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxSha(\d+)AndType agent\]$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[openidAndType agent\]$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[accountAndType agent\]$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[mbox\] is changed to '\[conformancetest@tincanapi\.com\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[mbox\] is changed to '\[bad mbox\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[objectType\] is changed to '\[notAgent\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[openid\] is changed to '\[bad openid\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[objectType\] is changed to '\[agent\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[account homePage\] is changed to '\[bad homePage\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[mbox\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[mbox_sha(\d+)sum\] is removed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[openid\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[account\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[account name\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[account homePage\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxOnly agent\]$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxSha(\d+)Only agent\]$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[openidOnly agent\]$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[accountOnly agent\]$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
---
+++ features/xapi/statement_structure/object_agent.feature
@@ -0,0 +1,54 @@
+# encoding: UTF-8
+Feature: statement structure object agent test
+
+  Scenario Outline: Good object agent: [<object>]
+
+    Given a [<type>] saveStatement request
+    Given the statement object is changed to a [<object>]
+    When the request is made
+    Then the request was successful
+
+  Examples:
+    | type    | object                    |
+    | typical | mboxAndType agent         |
+    | typical | mboxSha1AndType agent     |
+    | typical | openidAndType agent       |
+    | typical | accountAndType agent      |
+
+  Scenario Outline: Bad object agent: [<object>] with bad [<property>] '[<value>]'
+
+    Given a [<type>] saveStatement request
+    Given the statement object is changed to a [<object>]
+    Given the statement object [<property>] is changed to '[<value>]'
+    When the request is made
+    Then the LRS responds with HTTP [<HTTP>]
+
+  Examples:
+    | HTTP | type    | object                                | property                  | value                          |
+    | 400  | typical | mboxAndType agent                     | mbox                      | conformancetest@tincanapi.com  |
+    | 400  | typical | mboxAndType agent                     | mbox                      | bad mbox                       |
+    | 400  | typical | mboxAndType agent                     | objectType                | notAgent                       |
+    | 400  | typical | openidAndType agent                   | openid                    | bad openid                     |
+    | 400  | typical | mboxAndType agent                     | objectType                | agent                          |
+    | 400  | typical | accountAndType agent                  | account homePage          | bad homePage                   |
+
+  Scenario Outline: Bad object agent: [<object>] missing [<property>]
+
+    Given a [<type>] saveStatement request
+    Given the statement object is changed to a [<object>]
+    Given the statement object [<property>] is removed
+    When the request is made
+    Then the LRS responds with HTTP [<HTTP>]
+
+  Examples:
+    | HTTP | type    | object                             | property            |
+    | 400  | typical | mboxAndType agent                  | mbox                |
+    | 400  | typical | mboxSha1AndType agent              | mbox_sha1sum        |
+    | 400  | typical | openidAndType agent                | openid              |
+    | 400  | typical | accountAndType agent               | account             |
+    | 400  | typical | accountAndType agent               | account name        |
+    | 400  | typical | accountAndType agent               | account homePage    |
+    | 400  | typical | mboxOnly agent                     | mbox                |
+    | 400  | typical | mboxSha1Only agent                 | mbox_sha1sum        |
+    | 400  | typical | openidOnly agent                   | openid              |
+    | 400  | typical | accountOnly agent                  | account             |
