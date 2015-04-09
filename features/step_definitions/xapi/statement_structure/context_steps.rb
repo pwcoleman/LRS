df5ec1f294102cd386d86d3f44f1e5ec0091d1ac
Paul Coleman <pcoleman@coleman.co.uk>
2015-04-09 10:27:20 +0100

    added statement structure context feature

---
+++ features/step_definitions/xapi/statement_structure/context_steps.rb
@@ -0,0 +1,269 @@
+# encoding: UTF-8
+Given(/^a typical saveStatement request$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[empty\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[typical\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[mboxAndTypeAgentInstructor\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[mboxSha(\d+)AndTypeAgentInstructor\] context$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[openidAndTypeAgentInstructor\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[accountAndTypeAgentInstructor\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[extensionsOnly\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[emptyExtensionsOnly\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[emptyContextActivities\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[emptyContextActivitiesAllPropertiesEmpty\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[allProperties\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor\] is changed to \[an empty agent\]$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[team\] is changed to \[an empty group\]$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[language\] is changed to \[an emptyString content\]$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[statement\] is changed to \[an empty statementRef\]$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[registration\] is changed to '\[bad id\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor\] is changed to '\[notAnAgent\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor mbox\] is changed to '\[conformancetest@tincanapi\.com\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor mbox\] is changed to '\[bad mbox\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor objectType\] is changed to '\[notAgent\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[typicalGroupTeam\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[team objectType\] is changed to '\[notAGroup\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[team mbox\] is changed to '\[conformancetest@tincanapi\.com\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[team mbox\] is changed to '\[bad mbox\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[language\] is changed to '\[bad language\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[statementOnly\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[statement id\] is changed to '\[bad id\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[statement objectType\] is changed to '\[notStatementRef\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[statement objectType\] is changed to '\[statementRef\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor openid\] is changed to '\[bad openid\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor account homePage\] is changed to '\[bad homePage\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[typicalAgentInstructor\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor objectType\] is changed to '\[agent\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[team objectType\] is changed to '\[group\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context is changed to a \[contextActivitiesAllPropertiesOnly\] context$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities category (\d+) objectType\] is changed to '\[notActivity\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities parent (\d+) objectType\] is changed to '\[notActivity\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities grouping (\d+) objectType\] is changed to '\[notActivity\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities other (\d+) objectType\] is changed to '\[notActivity\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities category (\d+) id\] is changed to '\[bad id\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities parent (\d+) id\] is changed to '\[bad id\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities grouping (\d+) id\] is changed to '\[bad id\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities other (\d+) id\] is changed to '\[bad id\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities category (\d+) objectType\] is changed to '\[activity\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities parent (\d+) objectType\] is changed to '\[activity\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities grouping (\d+) objectType\] is changed to '\[activity\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities other (\d+) objectType\] is changed to '\[activity\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[extensions badkey\] is changed to '\[some value\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor mbox\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor mbox_sha(\d+)sum\] is removed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor openid\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor account\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor account name\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[instructor account homePage\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[team mbox\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[statement id\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[team objectType\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[statement objectType\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities category (\d+) id\] is removed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities parent (\d+) id\] is removed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities grouping (\d+) id\] is removed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities other (\d+) id\] is removed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities category (\d+) objectType\] is removed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities parent (\d+) objectType\] is removed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities grouping (\d+) objectType\] is removed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement context \[contextActivities other (\d+) objectType\] is removed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
---
+++ features/xapi/statement_structure/context.feature
@@ -0,0 +1,140 @@
+# encoding: UTF-8
+Feature: statement structure context test
+
+  Scenario Outline: Good context: [<modifier>] context
+
+    Given a typical saveStatement request
+    Given the statement context is changed to a [<modifier>] context
+    When the request is made
+    Then the request was successful
+
+  Examples:
+    | modifier                                  |
+    | empty                                     |
+    | typical                                   |
+    | mboxAndTypeAgentInstructor                |
+    | mboxSha1AndTypeAgentInstructor            |
+    | openidAndTypeAgentInstructor              |
+    | accountAndTypeAgentInstructor             |
+    | extensionsOnly                            |
+    | emptyExtensionsOnly                       |
+    | emptyContextActivities                    |
+    | emptyContextActivitiesAllPropertiesEmpty  |
+    | allProperties                             |
+
+  Scenario Outline: Bad context: [<modifier>] context with bad [<property>] [<value>]
+
+    Given a [<type>] saveStatement request
+    Given the statement context is changed to a [<modifier>] context
+    Given the statement context [<property>] is changed to [<value>]
+    When the request is made
+    Then the LRS responds with HTTP [<HTTP>]
+
+  Examples:
+    | HTTP | type    | modifier                | property                                | value                     |
+    | 400  | typical | empty                   | instructor                              | an empty agent            |
+    | 400  | typical | empty                   | team                                    | an empty group            |
+    | 400  | typical | empty                   | language                                | an emptyString content    |
+    | 400  | typical | empty                   | statement                               | an empty statementRef     |
+
+  Scenario Outline: Bad context: [<modifier>] context with bad [<property>] '[<value>]'
+
+    Given a [<type>] saveStatement request
+    Given the statement context is changed to a [<modifier>] context
+    Given the statement context [<property>] is changed to '[<value>]'
+    When the request is made
+    Then the LRS responds with HTTP [<HTTP>]
+
+  Examples:
+    | HTTP | type    | modifier                           | property                                | value                               |
+    | 400  | typical | empty                              | registration                            | bad id                              |
+    | 400  | typical | empty                              | instructor                              | notAnAgent                          |
+    | 400  | typical | mboxAndTypeAgentInstructor         | instructor mbox                         | conformancetest@tincanapi.com       |
+    | 400  | typical | mboxAndTypeAgentInstructor         | instructor mbox                         | bad mbox                            |
+    | 400  | typical | mboxAndTypeAgentInstructor         | instructor objectType                   | notAgent                            |
+    | 400  | typical | typicalGroupTeam                   | team objectType                         | notAGroup                           |
+    | 400  | typical | typicalGroupTeam                   | team mbox                               | conformancetest@tincanapi.com       |
+    | 400  | typical | typicalGroupTeam                   | team mbox                               | bad mbox                            |
+    | 400  | typical | empty                              | language                                | bad language                        |
+    | 400  | typical | statementOnly                      | statement id                            | bad id                              |
+    | 400  | typical | statementOnly                      | statement objectType                    | notStatementRef                     |
+    | 400  | typical | statementOnly                      | statement objectType                    | statementRef                        |
+    | 400  | typical | openidAndTypeAgentInstructor       | instructor openid                       | bad openid                          |
+    | 400  | typical | accountAndTypeAgentInstructor      | instructor account homePage             | bad homePage                        |
+    | 400  | typical | typicalAgentInstructor             | instructor objectType                   | agent                               |
+    | 400  | typical | typicalGroupTeam                   | team objectType                         | group                               |
+    | 400  | typical | contextActivitiesAllPropertiesOnly | contextActivities category 0 objectType | notActivity                         |
+    | 400  | typical | contextActivitiesAllPropertiesOnly | contextActivities parent 0 objectType   | notActivity                         |
+    | 400  | typical | contextActivitiesAllPropertiesOnly | contextActivities grouping 0 objectType | notActivity                         |
+    | 400  | typical | contextActivitiesAllPropertiesOnly | contextActivities other 0 objectType    | notActivity                         |
+    | 400  | typical | contextActivitiesAllPropertiesOnly | contextActivities category 0 id         | bad id                              |
+    | 400  | typical | contextActivitiesAllPropertiesOnly | contextActivities parent 0 id           | bad id                              |
+    | 400  | typical | contextActivitiesAllPropertiesOnly | contextActivities grouping 0 id         | bad id                              |
+    | 400  | typical | contextActivitiesAllPropertiesOnly | contextActivities other 0 id            | bad id                              |
+    | 400  | typical | contextActivitiesAllPropertiesOnly | contextActivities category 0 objectType | activity                            |
+    | 400  | typical | contextActivitiesAllPropertiesOnly | contextActivities parent 0 objectType   | activity                            |
+    | 400  | typical | contextActivitiesAllPropertiesOnly | contextActivities grouping 0 objectType | activity                            |
+    | 400  | typical | contextActivitiesAllPropertiesOnly | contextActivities other 0 objectType    | activity                            |
+    | 400  | typical | extensionsOnly                     | extensions badkey                       | some value                          |
+
+  Scenario Outline: Bad context: [<modifier>] context missing [<property>]
+
+    Given a [<type>] saveStatement request
+    Given the statement context is changed to a [<modifier>] context
+    Given the statement context [<property>] is removed
+    When the request is made
+    Then the LRS responds with HTTP [<HTTP>]
+
+  Examples:
+    | HTTP | type    | modifier                       | property                      |
+    | 400  | typical | mboxAndTypeAgentInstructor     | instructor mbox               |
+    | 400  | typical | mboxSha1AndTypeAgentInstructor | instructor mbox_sha1sum       |
+    | 400  | typical | openidAndTypeAgentInstructor   | instructor openid             |
+    | 400  | typical | accountAndTypeAgentInstructor  | instructor account            |
+    | 400  | typical | accountAndTypeAgentInstructor  | instructor account name       |
+    | 400  | typical | accountAndTypeAgentInstructor  | instructor account homePage   |
+    | 400  | typical | allProperties                  | team mbox                     |
+    | 400  | typical | allProperties                  | statement id                  |
+
+  Scenario Outline: Good context: [<modifier>] context missing [<property>]
+
+    Given a [<type>] saveStatement request
+    Given the statement context is changed to a [<modifier>] context
+    Given the statement context [<property>] is removed
+    When the request is made
+    Then the request was successful
+
+  Examples:
+    | type    | modifier      | property                |
+    | typical | allProperties | team objectType         |
+    | typical | allProperties | statement objectType    |
+
+  Scenario Outline: Bad context: [<modifier>] context missing [<property>]
+
+    Given a [<type>] saveStatement request
+    Given the statement context is changed to a [<modifier>] context
+    Given the statement context [<property>] is removed
+    When the request is made
+    Then the LRS responds with HTTP [<HTTP>]
+
+  Examples:
+    | HTTP | type    | modifier      | property                             |
+    | 400  | typical | allProperties | contextActivities category 0 id      |
+    | 400  | typical | allProperties | contextActivities parent 0 id        |
+    | 400  | typical | allProperties | contextActivities grouping 0 id      |
+    | 400  | typical | allProperties | contextActivities other 0 id         |
+
+  Scenario Outline: Good context: [<modifier>] context missing [<property>]
+
+    Given a [<type>] saveStatement request
+    Given the statement context is changed to a [<modifier>] context
+    Given the statement context [<property>] is removed
+    When the request is made
+    Then the request was successful
+
+  Examples:
+    | type    | modifier      | property                                    |
+    | typical | allProperties | contextActivities category 0 objectType     |
+    | typical | allProperties | contextActivities parent 0 objectType       |
+    | typical | allProperties | contextActivities grouping 0 objectType     |
+    | typical | allProperties | contextActivities other 0 objectType        |
