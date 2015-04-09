18fcbc3c2dabfa4346035efc9a1340d8ebf6a248
Paul Coleman <pcoleman@coleman.co.uk>
2015-04-09 11:37:45 +0100

    added statement structure object substatement feature

---
+++ features/step_definitions/xapi/statement_structure/object_sub_statement_steps.rb
@@ -0,0 +1,268 @@
+# encoding: UTF-8
+Given(/^the statement object is changed to a \[mboxOnlyAgentActor\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxSha(\d+)OnlyAgentActor\] subStatement$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[openidOnlyAgentActor\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[accountOnlyAgentActor\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxAndTypeAgentActor\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxSha(\d+)AndTypeAgentActor\] subStatement$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[openidAndTypeAgentActor\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[accountAndTypeAgentActor\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxAndTypeGroupActor\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxSha(\d+)AndTypeGroupActor\] subStatement$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[openidAndTypeGroupActor\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[accountAndTypeGroupActor\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[idOnlyVerb\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxAndTypeAgentObject\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxSha(\d+)AndTypeAgentObject\] subStatement$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[openidAndTypeAgentObject\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[accountAndTypeAgentObject\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxAndTypeGroupObject\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxSha(\d+)AndTypeGroupObject\] subStatement$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[openidAndTypeGroupObject\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[accountAndTypeGroupObject\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[allPropertiesTypicalAgentMemberGroupObject\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[allPropertiesActivityObject\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[typicalStatementRefObject\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[allProperties\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[objectType\] is set to '\[notSubStatement\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[objectType\] is set to '\[subStatement\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[actor mbox\] is set to '\[conformancetest@tincanapi\.com\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[actor mbox\] is set to '\[badMbox\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[actor openid\] is set to '\[bad openid\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[actor account homePage\] is set to '\[bad homePage\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[actor objectType\] is set to '\[notAgent\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[actor objectType\] is set to '\[agent\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[typical\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[verb id\] is set to '\[bad id\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxOnlyAgentObject\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object mbox\] is set to '\[conformancetest@tincanapi\.com\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object mbox\] is set to '\[badMbox\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[openidOnlyAgentObject\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object openid\] is set to '\[bad openid\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[accountOnlyAgentObject\] subStatement$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object account homePage\] is set to '\[bad homePage\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object objectType\] is set to '\[notAgent\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[objectType\] is set to '\[agent\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object member (\d+) mbox\] is set to '\[conformancetest@tincanapi\.com\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object member (\d+) mbox\] is set to '\[bad mbox\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object member (\d+) objectType\] is set to '\[notAgent\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object member (\d+) objectType\] is set to '\[agent\]'$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object objectType\] is set to '\[statementRef\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object objectType\] is set to '\[notStatementRef\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object id\] is set to '\[bad id\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[actor mbox\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[actor mbox_sha(\d+)sum\] is removed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[actor openid\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[actor account\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[actor account name\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[actor account homePage\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[verb id\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object mbox\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object mbox_sha(\d+)sum\] is removed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object openid\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object account\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object account name\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object account homePage\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[mboxSha(\d+)OnlyAgentObject\] subStatement$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object id\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[object objectType\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/xapi/statement_structure/object_sub_statement.feature
@@ -0,0 +1,111 @@
+# encoding: UTF-8
+Feature: statement structure object substatement test
+
+  Scenario Outline: Good object subStatement: [<modifier>] subStatement
+
+    Given a [<type>] saveStatement request
+    Given the statement object is changed to a [<modifier>] subStatement
+    When the request is made
+    Then the request was successful
+
+  Examples:
+    | type    | modifier                                    |
+    | typical | mboxOnlyAgentActor                          |
+    | typical | mboxSha1OnlyAgentActor                      |
+    | typical | openidOnlyAgentActor                        |
+    | typical | accountOnlyAgentActor                       |
+    | typical | mboxAndTypeAgentActor                       |
+    | typical | mboxSha1AndTypeAgentActor                   |
+    | typical | openidAndTypeAgentActor                     |
+    | typical | accountAndTypeAgentActor                    |
+    | typical | mboxAndTypeGroupActor                       |
+    | typical | mboxSha1AndTypeGroupActor                   |
+    | typical | openidAndTypeGroupActor                     |
+    | typical | accountAndTypeGroupActor                    |
+    | typical | idOnlyVerb                                  |
+    | typical | mboxAndTypeAgentObject                      |
+    | typical | mboxSha1AndTypeAgentObject                  |
+    | typical | openidAndTypeAgentObject                    |
+    | typical | accountAndTypeAgentObject                   |
+    | typical | mboxAndTypeGroupObject                      |
+    | typical | mboxSha1AndTypeGroupObject                  |
+    | typical | openidAndTypeGroupObject                    |
+    | typical | accountAndTypeGroupObject                   |
+    | typical | allPropertiesTypicalAgentMemberGroupObject  |
+    | typical | allPropertiesActivityObject                 |
+    | typical | typicalStatementRefObject                   |
+    | typical | allProperties                               |
+
+  Scenario Outline: Bad object subStatement: [<modifier>] subStatement with bad [<property>] '[<value>]'
+
+    Given a [<type>] saveStatement request
+    Given the statement object is changed to a [<modifier>] subStatement
+    Given the statement object [<property>] is set to '[<value>]'
+    When the request is made
+    Then the LRS responds with HTTP [<HTTP>]
+
+  Examples:
+    | HTTP | type    | modifier                                   | property                   | value                          |
+    | 400  | typical | allProperties                              | objectType                 | notSubStatement                |
+    | 400  | typical | allProperties                              | objectType                 | subStatement                   |
+    | 400  | typical | mboxOnlyAgentActor                         | actor mbox                 | conformancetest@tincanapi.com  |
+    | 400  | typical | mboxOnlyAgentActor                         | actor mbox                 | badMbox                        |
+    | 400  | typical | openidOnlyAgentActor                       | actor openid               | bad openid                     |
+    | 400  | typical | accountOnlyAgentActor                      | actor account homePage     | bad homePage                   |
+    | 400  | typical | mboxAndTypeAgentActor                      | actor objectType           | notAgent                       |
+    | 400  | typical | mboxAndTypeAgentActor                      | actor objectType           | agent                          |
+    | 400  | typical | typical                                    | verb id                    | bad id                         |
+    | 400  | typical | mboxOnlyAgentObject                        | object mbox                | conformancetest@tincanapi.com  |
+    | 400  | typical | mboxOnlyAgentObject                        | object mbox                | badMbox                        |
+    | 400  | typical | openidOnlyAgentObject                      | object openid              | bad openid                     |
+    | 400  | typical | accountOnlyAgentObject                     | object account homePage    | bad homePage                   |
+    | 400  | typical | mboxAndTypeAgentObject                     | object objectType          | notAgent                       |
+    | 400  | typical | mboxAndTypeAgentObject                     | objectType                 | agent                          |
+    | 400  | typical | allPropertiesTypicalAgentMemberGroupObject | object member 0 mbox       | conformancetest@tincanapi.com  |
+    | 400  | typical | allPropertiesTypicalAgentMemberGroupObject | object member 0 mbox       | bad mbox                       |
+    | 400  | typical | allPropertiesTypicalAgentMemberGroupObject | object member 0 objectType | notAgent                       |
+    | 400  | typical | allPropertiesTypicalAgentMemberGroupObject | object member 0 objectType | agent                          |
+    | 400  | typical | typicalStatementRefObject                  | object objectType          | statementRef                   |
+    | 400  | typical | typicalStatementRefObject                  | object objectType          | notStatementRef                |
+    | 400  | typical | typicalStatementRefObject                  | object id                  | bad id                         |
+
+  Scenario Outline: Bad object subStatement: [<modifier>] subStatement missing [<property>]
+
+    Given a [<type>] saveStatement request
+    Given the statement object is changed to a [<modifier>] subStatement
+    Given the statement object [<property>] is removed
+    When the request is made
+    Then the LRS responds with HTTP [<HTTP>]
+
+  Examples:
+    | HTTP | type    | modifier                    | property                   |
+    | 400  | typical | mboxAndTypeAgentActor       | actor mbox                 |
+    | 400  | typical | mboxSha1AndTypeAgentActor   | actor mbox_sha1sum         |
+    | 400  | typical | openidAndTypeAgentActor     | actor openid               |
+    | 400  | typical | accountAndTypeAgentActor    | actor account              |
+    | 400  | typical | accountAndTypeAgentActor    | actor account name         |
+    | 400  | typical | accountAndTypeAgentActor    | actor account homePage     |
+    | 400  | typical | mboxOnlyAgentActor          | actor mbox                 |
+    | 400  | typical | mboxSha1OnlyAgentActor      | actor mbox_sha1sum         |
+    | 400  | typical | openidOnlyAgentActor        | actor openid               |
+    | 400  | typical | accountOnlyAgentActor       | actor account              |
+    | 400  | typical | mboxAndTypeGroupActor       | actor mbox                 |
+    | 400  | typical | mboxSha1AndTypeGroupActor   | actor mbox_sha1sum         |
+    | 400  | typical | openidAndTypeGroupActor     | actor openid               |
+    | 400  | typical | accountAndTypeGroupActor    | actor account              |
+    | 400  | typical | accountAndTypeGroupActor    | actor account name         |
+    | 400  | typical | accountAndTypeGroupActor    | actor account homePage     |
+    | 400  | typical | typical                     | verb id                    |
+    | 400  | typical | mboxAndTypeAgentObject      | object mbox                |
+    | 400  | typical | mboxSha1AndTypeAgentObject  | object mbox_sha1sum        |
+    | 400  | typical | openidAndTypeAgentObject    | object openid              |
+    | 400  | typical | accountAndTypeAgentObject   | object account             |
+    | 400  | typical | accountAndTypeAgentObject   | object account name        |
+    | 400  | typical | accountAndTypeAgentObject   | object account homePage    |
+    | 400  | typical | mboxOnlyAgentObject         | object mbox                |
+    | 400  | typical | mboxSha1OnlyAgentObject     | object mbox_sha1sum        |
+    | 400  | typical | openidOnlyAgentObject       | object openid              |
+    | 400  | typical | accountOnlyAgentObject      | object account             |
+    | 400  | typical | typicalStatementRefObject   | object id                  |
+    | 400  | typical | typicalStatementRefObject   | object objectType          |
+    | 400  | typical | allPropertiesActivityObject | object id                  |
