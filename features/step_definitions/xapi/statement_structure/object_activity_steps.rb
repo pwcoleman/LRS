4cb33fb73b021ac7f8a1cbf7a2827cd4608d2c21
Paul Coleman <pcoleman@coleman.co.uk>
2015-04-09 11:10:58 +0100

    added statement structure object activity feature

---
+++ features/step_definitions/xapi/statement_structure/object_activity_steps.rb
@@ -0,0 +1,116 @@
+# encoding: UTF-8
+Given(/^the statement object is changed to a \[idOnly\] activity$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[idAndObjectType\] activity$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[idAndDefinition\] activity$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object is changed to a \[allProperties\] activity$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[id\] is changed to '\[bad id\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[objectType\] is changed to '\[notActivity\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[objectType\] is changed to '\[activity\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[definition type\] is changed to '\[bad IRI\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[definition interactionType\] is changed to '\[bad interaction\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[definition moreInfo\] is changed to '\[bad IRL\]'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object \[id\] is removed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^an activity saveStatement request$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[nameOnly\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[descriptionOnly\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[typeOnly\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[moreInfoOnly\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[extensionsOnly\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[emptyExtensionsOnly\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[allProperties\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[trueFalse\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[fillIn\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[numeric\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[other\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[otherWithCorrectResponsesPattern\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[choice\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[sequencing\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[likert\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[matching\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the statement object definition is set to a \[performance\] activityDefinition$/) do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/xapi/statement_structure/object_activity.feature
@@ -0,0 +1,72 @@
+# encoding: UTF-8
+Feature: statement structure object activity test
+
+  Scenario Outline: Good object activity: [<modifier>] activity
+
+    Given a [<type>] saveStatement request
+    Given the statement object is changed to a [<modifier>] activity
+    When the request is made
+    Then the request was successful
+
+  Examples:
+    | type    | modifier          |
+    | typical | idOnly            |
+    | typical | idAndObjectType   |
+    | typical | idAndDefinition   |
+    | typical | allProperties     |
+
+  Scenario Outline: Bad object activity: [<modifier>] activity with bad [<property>] '[<value>]'
+
+    Given a typical saveStatement request
+    Given the statement object is changed to a [<modifier>] activity
+    Given the statement object [<property>] is changed to '[<value>]'
+    When the request is made
+    Then the LRS responds with HTTP 400
+
+  Examples:
+    | modifier      | property                   | value              |
+    | allProperties | id                         | bad id             |
+    | allProperties | objectType                 | notActivity        |
+    | allProperties | objectType                 | activity           |
+    | allProperties | definition type            | bad IRI            |
+    | allProperties | definition interactionType | bad interaction    |
+    | allProperties | definition moreInfo        | bad IRL            |
+
+  Scenario Outline: Bad object activity: [<modifier>] activity missing [<property>]
+
+    Given a [<type>] saveStatement request
+    Given the statement object is changed to a [<modifier>] activity
+    Given the statement object [<property>] is removed
+    When the request is made
+    Then the LRS responds with HTTP [<HTTP>]
+
+  Examples:
+    | HTTP | type    | modifier      | property   |
+    | 400  | typical | allProperties | id         |
+
+  Scenario Outline: Good activity definition permutation: [<modifier>] definition
+
+    Given an activity saveStatement request
+    Given the statement object definition is set to a [<modifier>] activityDefinition
+    When the request is made
+    Then the request was successful
+
+  Examples:
+    | modifier                          |
+    | nameOnly                          |
+    | descriptionOnly                   |
+    | typeOnly                          |
+    | moreInfoOnly                      |
+    | extensionsOnly                    |
+    | emptyExtensionsOnly               |
+    | allProperties                     |
+    | trueFalse                         |
+    | fillIn                            |
+    | numeric                           |
+    | other                             |
+    | otherWithCorrectResponsesPattern  |
+    | choice                            |
+    | sequencing                        |
+    | likert                            |
+    | matching                          |
+    | performance                       |
