7c938ea43dac7f152dab5c1ff92b9070d5a1758d
Paul Coleman <pcoleman@coleman.co.uk>
2015-05-01 12:40:08 +0100

    Feature: Retrieve state
      Scenario Outline: Good retrieve state: [<type>] request cluster with agent parameter set to an [<value>] agent
    
      clean up

--- features/step_definitions/xapi/agent_profile/delete_agent_profile_steps.rb
+++ features/step_definitions/xapi/agent_profile/delete_agent_profile_steps.rb
@@ -16,39 +16,6 @@ Given(/^all requests' agent parameter are set to an \[(\w+)\] agent$/) do |type|
   @cluster['main']['params']['agent'] = agent
 end
 
-# Given(/^all requests' agent parameter are set to an \[mboxSha1AndType\] agent$/) do
-#   agent = Property::Agent.build('mboxSha1AndType')
-#   @cluster['primers'].each do |primer|
-#     primer.agent = agent
-#     primer.save
-#   end
-#   @cluster['main']['params']['agent'] = agent
-# end
-#
-# Given(/^all requests' agent parameter are set to an \[openidAndType\] agent$/) do
-#   pending # express the regexp above with the code you wish you had
-# end
-#
-# Given(/^all requests' agent parameter are set to an \[accountAndType\] agent$/) do
-#   pending # express the regexp above with the code you wish you had
-# end
-#
-# Given(/^all requests' agent parameter are set to an \[mboxOnly\] agent$/) do
-#   pending # express the regexp above with the code you wish you had
-# end
-#
-# Given(/^all requests' agent parameter are set to an \[mboxSha(\d+)Only\] agent$/) do |arg1|
-#   pending # express the regexp above with the code you wish you had
-# end
-#
-# Given(/^all requests' agent parameter are set to an \[openidOnly\] agent$/) do
-#   pending # express the regexp above with the code you wish you had
-# end
-#
-# Given(/^all requests' agent parameter are set to an \[accountOnly\] agent$/) do
-#   pending # express the regexp above with the code you wish you had
-# end
-
 Given(/^a typical deleteAgentProfile request$/) do
   pending # express the regexp above with the code you wish you had
 end
